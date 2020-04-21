19b0b87646682c5ef0fe486bcbc5d7b966da1ab2
Sam Nachman <shnachman@gmail.com>
2020-04-20 20:04:01 -0400

    added todo

--- Tweeter.py
+++ Tweeter.py
@@ -1,8 +1,15 @@
 import tweepy
 import keys
+import sys
+
+
 
 def tweet(message):
-    auth = tweepy.OAuthHandler(keys.CONSUMER_KEY, keys.CONSUMER_SECRET)
-    auth.set_access_token(keys.ACCESS_TOKEN, keys.ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth)
-    api.update_status(message)
+    try:
+        auth = tweepy.OAuthHandler(keys.CONSUMER_KEY, keys.CONSUMER_SECRET)
+        auth.set_access_token(keys.ACCESS_TOKEN, keys.ACCESS_TOKEN_SECRET)
+        api = tweepy.API(auth)
+        api.update_status(message)
+    except tweepy.TweepError as e:
+        # TODO: Add logging
+        print("Duplicate Tweet")
